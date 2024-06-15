0f30a80c7abb7d4658ad6f2e51dec5a8acdddb4d
lunarinaa <aydan.akbarova@gmail.com>
2024-06-15 14:30:10 +0400

    content processor updated to handle cart total

--- holbies_bakery/cart/context_processors.py
+++ holbies_bakery/cart/context_processors.py
@@ -3,3 +3,13 @@ from .cart import Cart
 def cart(request):
     return{'cart': Cart(request)}
 
+def cart_quantity(request):
+    cart = Cart(request)
+    quantities = cart.get_quants()
+    total_quantity = 0
+    for item_id, quantity in quantities.items():
+        total_quantity += quantity
+
+    return {
+        'total_quantity': total_quantity
+    }
