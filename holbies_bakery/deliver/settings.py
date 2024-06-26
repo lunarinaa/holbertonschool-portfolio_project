8039e4ef8bc1bd441c036b27f0fcb59248eda385
lunarinaa <aydan.akbarova@gmail.com>
2024-06-26 09:41:16 +0400

    database  dump to postgesql

--- holbies_bakery/deliver/settings.py
+++ holbies_bakery/deliver/settings.py
@@ -103,8 +103,12 @@ WSGI_APPLICATION = 'deliver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'admin',
+        'PASSWORD': 'holberton',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
